5fe5220db8bf4744918a352738d813893460daaa
Marvens Mertune <marvensmertune@gmail.com>
2022-06-13 11:30:21 -0400

    add modal functionality
    The application has the ability to open and a modal when the user selects the "Read More" link and the ability to close the modal by selecting the "Go Back" link

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 const search = document.querySelector(".search-box");
+const displayArea = document.querySelector(".book-holder");
+const readMore = document.querySelector(".popup");
+const modalContainer = document.querySelector(".modal-container");
+
 const bookList = [];
+
+const OutputHTML = (bookResults) => {
+  const html = bookResults
+    .map(
+      (bookResults) => `
+    <div class="book-item">
+      <img src="${bookResults.imageLinks.smallThumbnail}" alt="book image">
+      <div class="info">
+        <p>Title: ${bookResults.title}</p>
+        <p>Author: ${bookResults.authors}</p>
+        <p>Description: ${bookResults.description.slice(0, 300)}</p>
+      </div>
+      <div class="links">
+        <a href="#">BookMark</a>
+        <a href="#" class="popup">Read More</a>
+      </div>
+    </div>
+  `
+    )
+    .join("");
+  displayArea.innerHTML = html;
+};
 
 const searchBook = async (searchInput) => {
   const clearList = () => {
@@ -11,6 +37,7 @@ const searchBook = async (searchInput) => {
       bookList.push(book.volumeInfo);
     }
     console.log(bookList);
+    OutputHTML(bookList);
   };
   // gets the javascript object of first 5 books
   const res = await fetch(
@@ -22,4 +49,33 @@ const searchBook = async (searchInput) => {
   await displaybook(searchResults.items);
 };
 
+const displayModal = () => {
+  if (modalContainer.style.display === "none" || modalContainer.style.display === "") {
+    modalContainer.style.display = "block";
+  } else {
+    modalContainer.style.display = "none";
+  }
+  
+};
+const closeModal = () =>{
+  
+  if (modalContainer.style.display === "block") {
+    modalContainer.style.display = "none";
+  } else {
+    modalContainer.style.display = "block";
+  }
+}
+
 search.addEventListener("input", () => searchBook(search.value));
+displayArea.addEventListener("click", function(e) {
+  console.log(e);
+  if(e.target.matches("a.popup")){
+    displayModal();
+  }
+})
+modalContainer.addEventListener('click', function(e){
+  console.log(e);
+  if(e.target.matches('a.go-back')){
+    closeModal();
+  }
+})
